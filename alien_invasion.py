78ebf47a5a8c883d726b6fc2376231e0f46e64d5
abdixaden <72987030+abdixaden@users.noreply.github.com>
2025-04-03 12:38:27 -0400

    created a ship class and set to draw screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,45 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
+class AlienInvasion:
+    
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_w,self.settings.screen_h)
+            )
+        pygame.display.set_caption(self.settings.name)
+        
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg,
+            (self.settings.screen_w, self.settings.screen_h)
+            )
+        
+        self.running = True
+        self.clock = pygame.time.Clock()
+        
+        self.ship = Ship(self)
+           
+    def run_game(self):
+        
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+                    sys.exit()
+                    
+            self.screen.blit(self.bg, (0,0))
+            self.ship.draw()
+        
+            pygame.display.flip()
+            self.clock.tick(self.settings.FPS)
+            
 if __name__ == '__main__':
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
+
