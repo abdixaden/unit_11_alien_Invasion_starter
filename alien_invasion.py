5d0fd34e0aabda6af6afcd19f58a180b2f949e29
abdixaden <72987030+abdixaden@users.noreply.github.com>
2025-04-03 13:26:43 -0400

    added event for ship movings L and R while staying in boundaries

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,14 +25,14 @@ class AlienInvasion:
         self.ship = Ship(self)
            
     def run_game(self):
-        
         while self.running:
-            self._check_events()
-                    
+            
+            self._check_events()       
             self._update_screen()
+            self.ship.update()
             self.clock.tick(self.settings.FPS)
 
-    def new_method(self):
+    def _update_screen(self):
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         pygame.display.flip()
@@ -43,7 +43,33 @@ class AlienInvasion:
                 self.running = False
                 pygame.quit()
                 sys.exit()
-            
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+                
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+    
+    def _check_keyup_events(self, event):
+        
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+    
+    
+    
+    
+    
+    
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
