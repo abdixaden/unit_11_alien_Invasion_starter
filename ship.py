390448e7200052d89610413e1dd7cdbf15c8818d
abdixaden <72987030+abdixaden@users.noreply.github.com>
2025-04-11 11:23:37 -0400

    sett up ship to reset when crashing with aliens

--- ship.py
+++ ship.py
@@ -4,46 +4,43 @@ from arsenal import Arsenal
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
+
 class Ship:
-    def __init__(self, game: "AlienInvasion", arsenal:'Arsenal'):
+    def __init__(self, game: "AlienInvasion", arsenal: 'Arsenal'):
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
         self.boundaries = self.screen.get_rect()
         
-         
         self.image = pygame.image.load(str(self.settings.ship_file))
-        self.image = pygame.transform.scale(
-            self.image, 
-            (self.settings.ship_w, self.settings.ship_h)
-        )
+        self.image = pygame.transform.scale(self.image, 
+                                            (self.settings.ship_w, self.settings.ship_h))
         
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.boundaries.midbottom
+        self.rect.midright = self.boundaries.midright  # Ship positioned at the right edge
         
         self.ship_speed = 5.0
         self.moving_right = False
         self.moving_left = False
         self.x = float(self.rect.x)
         self.arsenal = arsenal
-        
     
     def update(self):   
-        self._update_ship_movement()
-        self.arsenal.update_arsenal()
-
-    def _update_ship_movement(self):
-        if self.moving_right and self.rect.right < self.boundaries.right:
+        if self.moving_right and not self.moving_left:
             self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left > 0:
+        elif self.moving_left and not self.moving_right:
             self.x -= self.settings.ship_speed
-            
-         
         self.rect.x = int(self.x)
         
+        self.arsenal.update_arsenal()
+        
+    def center_ship(self):
+        self.rect.midright = self.boundaries.midright  # Keep ship at the right edge
+        self.x = float(self.rect.x)
+
     def draw(self):
         self.arsenal.draw()
         self.screen.blit(self.image, self.rect)
         
     def fire(self):
-        return self.arsenal.fire_bullet()
+        return self.arsenal.fire_bullet()  # arsenal.py
